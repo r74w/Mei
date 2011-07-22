80610a12d9ce0bb9dff46a6bc7d4ec6573f5a075
whacao <endofsummer1981@gmail.com>
2011-07-22 14:35:52 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imageboard::Application.config.secret_token = '38cad4d27e2193f1c7c8f4bc0141fb689bb49df6e41f0ed53a8ca2bb9edbb5c826f49312357c55a35ee79a24b39a91505ba8376abc21af69affbc5c7fe645122'
