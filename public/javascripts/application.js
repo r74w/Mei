78b0c3c63e2f661edbfb18561c4292f093faa2a3
whacao <endofsummer1981@gmail.com>
2011-08-10 16:18:54 +0800

    If replying at index, redirect to discussion page. If replying at discussion page, use js to insert post id reference

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,13 @@
 $(function(){
-  $('.action_menu a.reply').click(function(){
-    var t = ">" + $(this).parent().parent().attr('id').substring(1) + "\n";
-    var ta = $('form textarea').first();
-    if(ta.val())
-      t = ta.val() + "\n" + t;
-    ta.val(t).focus();
-    ta[0].scrollTop = ta[0].scrollHeight;
-  });
+  if($('form.edit_discussion').length){
+    $('.action_menu a.reply').click(function(e){
+      e.preventDefault();
+      var t = ">" + $(this).parent().parent().attr('id').substring(1) + "\n";
+      var ta = $('form textarea').first();
+      if(ta.val())
+        t = ta.val() + "\n" + t;
+      ta.val(t).focus();
+      ta[0].scrollTop = ta[0].scrollHeight;
+    });
+  }
 })
