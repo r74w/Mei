94b6343b34ef8fac9a6e839f3ebaab1cc050f278
whacao <endofsummer1981@gmail.com>
2011-08-10 15:37:18 +0800

    Insert post id to textarea for replying

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  $('.action_menu a.reply').click(function(){
+    var t = ">" + $(this).parent().parent().attr('id').substring(1) + "\n";
+    var ta = $('form textarea').first();
+    if(ta.val())
+      t = ta.val() + "\n" + t;
+    ta.val(t).focus();
+    ta[0].scrollTop = ta[0].scrollHeight;
+  });
+})
