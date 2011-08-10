c81d91ba54ca8f2bd5fd020ac0b0943b61488faa
whacao <endofsummer1981@gmail.com>
2011-08-10 16:48:19 +0800

    Highlight same page anchored element for clarity

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,4 +10,15 @@ $(function(){
       ta[0].scrollTop = ta[0].scrollHeight;
     });
   }
+  $('a[href*=#]').click(function(){
+      var elemId = '#' + $(this).attr('href').split('#')[1];
+      highlight(elemId);
+  });
 })
+
+function highlight(elemId){
+    var elem = $(elemId);
+    elem.css("backgroundColor", "#ffffff"); // hack for Safari
+    elem.animate({ backgroundColor: '#ffffaa' }, 1500);
+    setTimeout(function(){$(elemId).animate({ backgroundColor: "#ffffff" }, 3000)},1000);
+}
